795a1bbf0adff9b708c2cb01d6fa38cf70554b31
TharunSivamani <tharunsivamani@gmail.com>
2023-07-13 09:58:23 +0530

    backprop.py

---
+++ Session 10/backprop.py
@@ -0,0 +1,146 @@
+import torch
+from tqdm import tqdm
+from utils import denormalize
+import matplotlib.pyplot as plt
+import numpy as np
+
+def GetCorrectPredCount(pPrediction, pLabels):
+    return pPrediction.argmax(dim=1).eq(pLabels).sum().item()
+
+def train(model, device, train_loader, optimizer, criterion, scheduler, train_losses, train_acc):
+    model.train()
+    pbar = tqdm(train_loader)
+
+    train_loss = 0
+    correct = 0
+    processed = 0
+
+    for batch_idx, (data, target) in enumerate(pbar):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+
+        # Predict
+        pred = model(data)
+
+        # Calculate loss
+        loss = criterion(pred, target)
+        train_loss+=loss.item()
+
+        # Backpropagation
+        loss.backward()
+        optimizer.step()
+        scheduler.step()
+
+        correct += GetCorrectPredCount(pred, target)
+        processed += len(data)
+
+        pbar.set_description(desc= f'Train: Loss={loss.item():0.4f} Batch_id={batch_idx} Accuracy={100*correct/processed:0.2f}')
+
+    train_acc.append(100*correct/processed)
+    train_losses.append(train_loss/len(train_loader))
+
+    return train_losses, train_acc
+
+def test(model, device, test_loader, criterion, test_losses, test_acc):
+    model.eval()
+
+    test_loss = 0
+    correct = 0
+
+    with torch.no_grad():
+        for _, (data, target) in enumerate(test_loader):
+            data, target = data.to(device), target.to(device)
+
+            output = model(data)
+            test_loss += criterion(output, target).item()  # sum up batch loss
+
+            correct += GetCorrectPredCount(output, target)
+
+
+    test_loss /= len(test_loader.dataset)
+    test_acc.append(100. * correct / len(test_loader.dataset))
+    test_losses.append(test_loss)
+
+    print('Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(test_loss, correct, len(test_loader.dataset),100. * correct / len(test_loader.dataset)))
+
+    return test_losses, test_acc
+
+def plot_graphs(train_losses , train_acc , test_losses , test_acc):
+    
+    fig, axs = plt.subplots(2,2,figsize=(15,10))
+    axs[0, 0].plot(train_losses)
+    axs[0, 0].set_title("Training Loss")
+    axs[1, 0].plot(train_acc)
+    axs[1, 0].set_title("Training Accuracy")
+    axs[0, 1].plot(test_losses)
+    axs[0, 1].set_title("Test Loss")
+    axs[1, 1].plot(test_acc)
+    axs[1, 1].set_title("Test Accuracy")
+
+    return
+
+def get_misclassified_images(model , test_loader , device):
+        model.eval()
+
+        images = []
+        predictions = []
+        labels = []
+
+        with torch.no_grad():
+            for data, target in test_loader:
+                data, target = data.to(device), target.to(device)
+
+                output = model(data)
+
+                _, preds = torch.max(output, 1)
+
+                for i in range(len(preds)):
+                    if preds[i] != target[i]:
+                        images.append(data[i])
+                        predictions.append(preds[i])
+                        labels.append(target[i])
+
+        return images, predictions, labels
+
+def show_misclassified_images(
+    images,
+    predictions,
+    labels,
+    classes):
+    
+    fig = plt.figure(figsize=(20, 10))
+    for i in range(len(images)):
+        sub = fig.add_subplot(len(images) // 5, 5, i + 1)
+        image = images[i]
+        npimg = denormalize(image.cpu().numpy().squeeze())
+        plt.imshow(npimg, cmap="gray")
+        predicted = classes[predictions[i]]
+        correct = classes[labels[i]]
+        sub.set_title(
+            "Correct class: {}\nPredicted class: {}".format(correct, predicted)
+        )
+    plt.tight_layout()
+    plt.show()
+
+def accuracy_classes(model, classes, test_loader):
+
+    correct_pred = {classname: 0 for classname in classes}
+    total_pred = {classname: 0 for classname in classes}
+
+    with torch.no_grad():
+        for data in test_loader:
+            images, labels = data
+            images, labels = images.cuda(), labels.cuda()
+            outputs = model(images)
+            _, predictions = torch.max(outputs, 1)
+
+            for label, prediction in zip(labels, predictions):
+                if label == prediction:
+                    correct_pred[classes[label]] += 1
+                total_pred[classes[label]] += 1
+
+    for classname, correct_count in correct_pred.items():
+        accuracy = 100 * float(correct_count) / total_pred[classname]
+        print(f'Accuracy for class: {classname:5s} is {accuracy:.1f} %')
+
+
